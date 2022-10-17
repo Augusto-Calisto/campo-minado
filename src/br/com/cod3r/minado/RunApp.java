7d4804946f32ba12bda33d8e700c4e63a7a6310c
Augusto Calisto <augustoaquino007@gmail.com>
2022-10-17 14:56:31 -0300

    Criacao de pastas

---
+++ src/br/com/cod3r/minado/RunApp.java
@@ -0,0 +1,8 @@
+package br.com.cod3r.minado;
+
+public class RunApp {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ test/br/com/cod3r/minado/RunAppTest.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.minado;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RunAppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
