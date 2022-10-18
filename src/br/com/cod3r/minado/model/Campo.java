dd460368b7cc88338cc3699a2fa8909717428151
Augusto Calisto <augustoaquino007@gmail.com>
2022-10-18 10:16:44 -0300

    Implementacao de criar marcacao e abrir campo e devidos testes

--- src/br/com/cod3r/minado/model/Campo.java
+++ src/br/com/cod3r/minado/model/Campo.java
@@ -3,7 +3,8 @@ package br.com.cod3r.minado.model;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unused")
+import br.com.cod3r.minado.excecao.ExplosaoException;
+
 public class Campo {	
 	private final int linha;
 	private final int coluna;
@@ -20,18 +21,86 @@ public class Campo {
 		this.vizinhos = new ArrayList<>();
 	}
 
-	public boolean adicionarVizinho(Campo campo) {
-		int deltaLinha = Math.abs(this.linha - campo.linha);
-		int deltaColuna = Math.abs(this.coluna - campo.coluna);
+	public boolean adicionarVizinho(Campo campoVizinho) {
+		int deltaLinha = Math.abs(this.linha - campoVizinho.linha);
+		int deltaColuna = Math.abs(this.coluna - campoVizinho.coluna);
 		
 		int deltaGeral = deltaLinha + deltaColuna;
 		
 		if(deltaGeral == 1 || deltaGeral == 2) {
-			vizinhos.add(campo);
+			vizinhos.add(campoVizinho);
 			
 			return true;
 		}
 
 		return false;
+	}
+	
+	public void alternarMarcacao() {
+		if(!this.aberto) {
+			this.marcado = !this.marcado;
+		}
+	}
+	
+	public boolean abrir() {
+		if(!this.aberto && !this.marcado) {
+			this.aberto = true;
+			
+			if(this.minado) {
+				throw new ExplosaoException("Voce explodiu uma bomba :( \n\n FIM DE JOGO");
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(vizinho -> vizinho.abrir()); // recursao
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(vizinho -> vizinho.minado);
+	}
+
+	public boolean isAberto() {
+		return aberto;
+	}
+
+	public void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+
+	public boolean isMinado() {
+		return minado;
+	}
+
+	public void setMinado(boolean minado) {
+		this.minado = minado;
+	}
+
+	public boolean isMarcado() {
+		return marcado;
+	}
+
+	public void setMarcado(boolean marcado) {
+		this.marcado = marcado;
+	}
+
+	public List<Campo> getVizinhos() {
+		return vizinhos;
+	}
+
+	public void setVizinhos(List<Campo> vizinhos) {
+		this.vizinhos = vizinhos;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
 	}
 }
--- test/br/com/cod3r/minado/model/CampoTest.java
+++ test/br/com/cod3r/minado/model/CampoTest.java
@@ -1,11 +1,14 @@
 package br.com.cod3r.minado.model;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import br.com.cod3r.minado.excecao.ExplosaoException;
 
 class CampoTest {
 	private Campo campo;
@@ -16,8 +19,8 @@ class CampoTest {
 	}
 	
 	@Test
-	@DisplayName(value = "Retornar true se a distancia entre os campos for igual a 1")
-	void retornarVerdadeiroDistanciaIgual1() {
+	@DisplayName(value = "Retornar verdadeiro para vizinho direita se a distancia entre os campos for igual a 1")
+	void retornarVerdadeiro_DistanciaIgual_Um_VizinhoDireita() {
 		Campo vizinhoDireito = new Campo(2, 3);
 		
 		boolean isVizinho = campo.adicionarVizinho(vizinhoDireito);
@@ -26,8 +29,18 @@ class CampoTest {
 	}
 	
 	@Test
-	@DisplayName(value = "Retornar true se a distancia entre os campos for igual a 2")
-	void retornarVerdadeiroDistanciaIgual2() {
+	@DisplayName(value = "Retornar verdadeiro para vizinho esquerda se a distancia entre os campos for igual a 1")
+	void retornarVerdadeiro_DistanciaIgual_Um_VizinhoEsquerda() {
+		Campo vizinhoDireito = new Campo(2, 1);
+		
+		boolean isVizinho = campo.adicionarVizinho(vizinhoDireito);
+		
+		assertTrue(isVizinho);
+	}
+	
+	@Test
+	@DisplayName(value = "Retornar verdadeiro para vizinho diagonal direita se a distancia entre os campos for igual a 2")
+	void retornarVerdadeiro_DistanciaIgual_Dois_DiagonalDireita() {
 		Campo vizinhoDiagonalDireita = new Campo(1, 3);
 		
 		boolean isVizinho = campo.adicionarVizinho(vizinhoDiagonalDireita);
@@ -36,12 +49,66 @@ class CampoTest {
 	}
 	
 	@Test
-	@DisplayName(value = "Retornar falso se a distancia entre os campos for diferente de 1 ou 2")
-	void retornarFalsoSeDeltaForMaiorQueUmOuDois() {
+	@DisplayName(value = "Retornar verdadeiro para vizinho diagonal esquerda se a distancia entre os campos for igual a 2")
+	void retornarVerdadeiro_DistanciaIgual_Dois_DiagonalEsquerda() {
+		Campo vizinhoDiagonalEsquerda = new Campo(1, 1);
+		
+		boolean isVizinho = campo.adicionarVizinho(vizinhoDiagonalEsquerda);
+		
+		assertTrue(isVizinho);
+	}
+	
+	@Test
+	@DisplayName(value = "Retornar falso se a distancia entre o campo e o vizinho for diferente de 1 ou 2")
+	void retornarFalso_Se_DeltaForMaior_UmOuDois() {
 		Campo vizinho = new Campo(1, 4);
 		
 		boolean isVizinho = campo.adicionarVizinho(vizinho);
 		
 		assertFalse(isVizinho);
+	}
+	
+	@Test
+	@DisplayName(value = "Retornar verdadeiro quando estiver fechado (aberto = false), marcado = true")
+	void marcadoTemQueSerVerdadeiro_QuandoEstiverFechado() {
+		campo.setAberto(false);
+		
+		campo.alternarMarcacao();
+		
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	@DisplayName(value = "Lancar a excecao ExplosaoException quando clicar em um campo minado")
+	void lancarExplosaoException_QuandoClicarEmCampoMinado() {
+		campo.setAberto(false);
+		
+		campo.setMarcado(false);
+		
+		campo.setMinado(true);
+		
+		assertThrows(ExplosaoException.class, () -> campo.abrir());
+	}
+	
+	@Test
+	@DisplayName(value = "Retornar verdadeiro quando clicar em um campo e expandir a vizinhanca")
+	void retornarVerdadeiro_QuandoClicar_E_ExpandirVizinhanca() {
+		campo.setAberto(false);
+		
+		campo.setMarcado(false);
+		
+		campo.setMinado(false);
+		
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	@DisplayName(value = "Retornar falso quando campo estiver aberto ou marcado")
+	void retornarFalso_QuandoEstiverAberto_E_Marcado() {
+		campo.setAberto(true);
+		
+		campo.setMarcado(true);
+				
+		assertFalse(campo.abrir());
 	}
 }
